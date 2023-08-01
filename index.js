754e206fc645e6b49a863b7efdc0e09917dd4a8d
arpitagrawal21 <arpit76826@gmail.com>
2023-08-01 22:37:12 +0530

    replace now with today

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const today = new Date();
+const day = today.getDay();
+let hour = today.getHours();
+let minute = today.getMinutes();
+let second = today.getSeconds();
+const dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+let prepand = (hour>=12) ? "PM" : "AM";
+if(hour>12){
+    hour = hour-12;
+}
+if(hour==24){
+    hour = 0;
+    prepand="AM";
+}
+if(hour<10){
+  hour = "0"+hour; 
+}
+if(minute<10){
+  minute = "0"+minute;
+}
+if(second<10){
+  second = "0"+second;
+}
+
+const currentDay = dayName[day];
+const currentTime = `${hour}:${minute}:${second} ${prepand}`;
+
+
+console.log("Today is : "+ currentDay) 
+console.log("Current Time is : "+currentTime);
